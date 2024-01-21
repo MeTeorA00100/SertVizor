72b7e1c7ca8806eb9b99b0b06837310ba8918d06
MeTeorAIT <84034922+MeTeorA00100@users.noreply.github.com>
2024-01-22 00:53:46 +0700

    Add files via upload

---
+++ src/main/java/com/mangxst/sertvizor/SertVizor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mangxst.sertvizor;
+
+import com.mangxst.sertvizor.GUI.welcome;
+
+/**
+ *
+ * @author 1
+ */
+public class SertVizor {
+
+    public static void main(String[] args) {
+         new welcome().setVisible(true);
+    }
+}
